222747fe24819175856178f4c33b7749d4310e13
drose14789 <drosedoing@naver.com>
2025-11-13 17:43:37 +0900

    'dbms2장select문'

---
+++ source/04_DBMS/1113_ch02_SELECT.sql
@@ -0,0 +1,72 @@
+-- [II] SELECT문 - 조회
+
+-- 1. SELECT 문장 작성법
+SELECT * FROM TAB; -- 현 계정이 가지고 있는 테이블 정보(실행: CTRL+ENTER)
+SELECT * FROM DEPT; -- DEPT 테이블의 모든 열, 모든 행
+SELECT * FROM SALGRADE; -- SALGRADE 테이블의 모든 열, 모든 행
+SELECT * FROM EMP; -- EMP 테이블의 모든 열, 모든행
+
+-- 2. 특정 열만 출력
+DESC EMP;
+    -- EMP테이블의 구조
+SELECT EMPNO, ENAME, SAL, JOB, HIREDATE FROM EMP; -- EMP테이블의 SELECT절에 지정된 열만 출력
+SELECT EMPNO AS "사 번", ENAME AS "이름", SAL AS "급여", JOB AS "직책" 
+    FROM EMP; -- 열 이름에 별칭을 두는 경우 EX. 열이름 AS "별칭", 열이름
+SELECT EMPNO "사 번", ENAME 이름, SAL 급여, JOB
+    FROM EMP; -- 별칭에 SPACE가 없는 경우는 따옴표 생략가능
+
+-- 3. 특정 행 출력 : WHERE절(조건절) -- 비교연사자 : 같다(=), 다르다(!=, ^=, <>), >, ...
+SELECT EMPNO 사번, ENAME 이름, SAL 급여 FROM EMP;
+SELECT EMPNO NO, ENAME NAME, SAL FROM EMP WHERE SAL!=3000;
+SELECT EMPNO NO, ENAME NAME, SAL FROM EMP WHERE SAL^=3000;
+SELECT EMPNO NO, ENAME NAME, SAL FROM EMP WHERE SAL<>3000;
+    -- 비교연산자는 숫자, 문자, 날짜형 모두 가능
+    -- ex1. 사원이름이 'A', 'B', 'C'로 시작하는 사원의 모든 열(필드)
+    -- A < AA < AAA < AAAA < ... < B < BA < ... < C
+    SELECT * FROM EMP WHERE ENAME < 'D';
+    -- ex2. 81년도 이전에 입사한 사원의 모든 열(필드)
+    SELECT * FROM EMP WHERE HIREDATE < ' 81/01/01' ;
+    -- ex3. 부서번호(DEPTNO)가 10번인 사원의 모든 필드
+    SELECT * FROM EMP WHERE DEPTNO=10;
+    -- SQL문은 대소문자 구별없음. 데이터는 대소문자 구별
+    -- ex4. 이름(ENAME)이 SCOTT인 직원의 모든 데이터(필드)
+    SELECT * FROM EMP WHERE ENAME = 'SCOTT';
+    
+-- 4. WHERE절(조건절)에 논리연산자 : AND OR NOT
+    -- ex1. 급여(SAL)가 2000이상 3000이하인 직원의 모든 필드
+    SELECT * FROM EMP WHERE 2000 <= SAL AND SAL <= 3000;
+    -- ex2. 82년도 입사한 사원의 모든 필드
+    SELECT * FROM EMP WHERE HIREDATE >='82/01/01' AND HIREDATE<='82/12/31';
+    
+    -- 날짜 표기법 셋팅 (현재:RR/MM//DD)
+    ALTER SESSION SET NLS_DATE_FORMAT = 'RR/MM/DD';
+    SELECT * FROM EMP 
+        WHERE TO_CHAR(HIREDATE,'YYYY-MM-DD')>='1982-01-01' 
+            AND TO_CHAR(HIREDATE, 'YYYY-MM-DD')<='1982-12-31';
+    -- ex3. 부서번호가 10이 아닌 직원의 모든 필드
+    SELECT * FROM EMP WHERE DEPTNO != 10;
+    SELECT * FROM EMP WHERE NOT DEPTNO = 10;
+    
+-- 5. 산술연산자 (SELECT절, WHERE절, ORDER BY절)
+SELECT EMPNO, ENAME, SAL "예전월급", SAL*1.1 "현재월급" FROM EMP;
+    --ex1. 연봉이 10,000이상인 직원의 ENAME(이름), SAL(월급), 연봉(SAL*12) - 연봉순
+    SELECT ENAME, SAL , SAL*12 ANNUALSAL --(3)
+        FROM EMP                -- (1)
+        WHERE SAL*12 >= 10000   -- (2)
+        ORDER BY ANNUALSAL;     -- (4)
+    -- 산술연산의 결과는 NULL을 포함하면 결과도 NULL
+    -- NVL(NULL일 수도 있는 필드명, 대체값)을 이용 : 필드명과 대체값은 타입이 일치
+    -- ex2. 직원의 이름, 월급, 상여, 연봉(SAL*12+COMM)
+     SELECT ENAME, SAL , COMM, SAL*12+NVL(COMM,0) ANNUALSAL 
+        FROM EMP;
+    -- ex3. 모든 사원의 ENAME, MGR(상사사번)을 출력-상사사번이 없으면 'CEO'
+    SELECT ENAME, EMPNO, NVL(TO_CHAR(MGR),'CEO') FROM EMP;
+    DESC EMP;
+    
+-- 6. 연결연산자 (||) : 필드내용이나 문자를 연결
+SELECT ENAME || '은(는)' || JOB FROM EMP;
+
+-- 7. 중복제거(DISTINCT)
+SELECT DISTINCT JOB FROM EMP;
+SELECT DISTINCT DEPTNO FROM EMP;
+    
